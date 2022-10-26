5488ac6ad473ec93e6997475bd65140b6479fbed
Bencze Dániel <rgrengarhyped@gmail.com>
2022-10-26 11:07:32 +0200

    Resize added

--- GUI_2022_23_01_UHPYQ8/Render/Display.cs
+++ GUI_2022_23_01_UHPYQ8/Render/Display.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace GUI_2022_23_01_UHPYQ8.Render
 {
-    class Display
+    public class Display : FrameworkElement
     {
+        Size size;
+        public void Resize(Size size)
+        {
+            this.size = size;
+            InvalidateVisual();
+        }
     }
 }
