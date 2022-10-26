1ec086e69038c621f4068c5a34ea7d5f8fc3727c
Bencze Dániel <rgrengarhyped@gmail.com>
2022-10-26 10:56:35 +0200

    Player added

---
+++ GUI_2022_23_01_UHPYQ8/Model/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_2022_23_01_UHPYQ8.Model
+{
+    public class Player
+    {
+        public int Rank { get; set; } = 1;
+        public string Name { get; set; }
+        public DateTime WhenStarted { get; set; } = DateTime.Now;
+        public int Score { get; set; }
+    }
+}
