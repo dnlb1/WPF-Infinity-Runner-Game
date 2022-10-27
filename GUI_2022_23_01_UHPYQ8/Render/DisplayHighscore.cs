d866dd629564ea07b6e6c6adcf7058be1d550a6f
Fekete Dominik <feketedominik@stud.uni-obuda.hu>
2022-10-27 09:21:23 +0200

    DisplayHighscore created

---
+++ GUI_2022_23_01_UHPYQ8/Render/DisplayHighscore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_2022_23_01_UHPYQ8.Render
+{
+    internal class DisplayHighscore
+    {
+    }
+}
