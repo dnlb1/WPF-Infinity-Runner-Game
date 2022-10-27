503f49ae68e9c3d0e747b154cd81157cb9fab6b1
Bencze Dániel <rgrengarhyped@gmail.com>
2022-10-27 12:43:15 +0200

    Size added

--- GUI_2022_23_01_UHPYQ8/Logic/GameLogic.cs
+++ GUI_2022_23_01_UHPYQ8/Logic/GameLogic.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace GUI_2022_23_01_UHPYQ8.Logic
 {
     public class GameLogic
     {
-
+        Size size;
+        public void Resize(Size size)
+        {
+            this.size = size;
+        }
     }
 }
