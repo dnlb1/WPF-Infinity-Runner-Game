b5f051d05b0ec18a895cfd11dd47b9f24be23193
Bencze Dániel <rgrengarhyped@gmail.com>
2022-10-27 13:01:24 +0200

    IGameLogic changed

--- GUI_2022_23_01_UHPYQ8/Logic/IGameLogic.cs
+++ GUI_2022_23_01_UHPYQ8/Logic/IGameLogic.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Media;
+using static GUI_2022_23_01_UHPYQ8.Logic.GameLogic;
 
 namespace GUI_2022_23_01_UHPYQ8.Logic
 {
     public interface IGameLogic
     {
+        void Control(ControlKey key);
         void Resize(Size size);
         string Name { get; set; }
         bool Intro { get; set; }
