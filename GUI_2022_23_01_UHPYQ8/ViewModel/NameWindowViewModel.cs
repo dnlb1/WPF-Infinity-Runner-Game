885e0b26ce0b8c3d893efdbda63035438a6f8329
Sörös Bence <bence.soros@stud.uni-obuda.hu>
2022-10-27 10:55:23 +0200

    ICommand Ok

--- GUI_2022_23_01_UHPYQ8/ViewModel/NameWindowViewModel.cs
+++ GUI_2022_23_01_UHPYQ8/ViewModel/NameWindowViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace GUI_2022_23_01_UHPYQ8.ViewModel
 {
@@ -17,6 +18,9 @@ namespace GUI_2022_23_01_UHPYQ8.ViewModel
         {
             get { return enteredName; }
             set { enteredName = value;  }
+
         }
+
+        public ICommand Ok { get; set; }
     }
 }
