f166d0d35e1970ab406d3c1856580fbc2288649a
Bencze Dániel <rgrengarhyped@gmail.com>
2022-10-27 12:37:04 +0200

    Name class

---
+++ GUI_2022_23_01_UHPYQ8/Service/Name.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_2022_23_01_UHPYQ8.Service
+{
+    public class Name 
+    {
+        public bool DialogResult { get; set; }
+        public string GetName()
+        {
+            YourName nw = new YourName();
+            nw.ShowDialog();
+            DialogResult = (bool)nw.DialogResult;
+            return nw.VMName.EnteredName;
+        }
+    }
+}
