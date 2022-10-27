fce81e4d4d930b85242076b1df40ebfd0c42f9d4
Fekete Dominik <feketedominik@stud.uni-obuda.hu>
2022-10-27 10:08:31 +0200

    runner collection created

--- GUI_2022_23_01_UHPYQ8/ViewModel/HighscoreViewModel.cs
+++ GUI_2022_23_01_UHPYQ8/ViewModel/HighscoreViewModel.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using GUI_2022_23_01_UHPYQ8.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace GUI_2022_23_01_UHPYQ8.ViewModel
 {
     public class HighscoreViewModel
     {
+        public ObservableCollection<Player> Runners { get; set; }
+        public static bool IsInDesign
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
     }
 }
