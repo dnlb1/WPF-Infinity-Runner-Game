aa0e911356cec9ebfd33ebee52da63c88c7891ba
Fekete Dominik <feketedominik@stud.uni-obuda.hu>
2022-10-27 10:12:34 +0200

    ranklist backend created

--- GUI_2022_23_01_UHPYQ8/ViewModel/HighscoreViewModel.cs
+++ GUI_2022_23_01_UHPYQ8/ViewModel/HighscoreViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,35 @@ namespace GUI_2022_23_01_UHPYQ8.ViewModel
             {
                 var prop = DesignerProperties.IsInDesignModeProperty;
                 return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+        public HighscoreViewModel()
+        {
+            if (!IsInDesign)
+            {
+                Runners = new ObservableCollection<Player>();
+                //valahonnan majd betöltjük a ranglistát
+                //Azokból létrehozunk 1 runner tömböt
+                if (File.Exists(Path.Combine("RankList", "Rank.txt")))
+                {
+                    string[] datas = File.ReadAllLines(Path.Combine("RankList", "Rank.txt"));
+
+                    for (int i = 0; i < datas.Length; i++)
+                    {
+                        string[] split = datas[i].Split("#");
+                        Player r = new Player();
+                        r.Rank = int.Parse(split[0]);
+                        r.WhenStarted = DateTime.Parse(split[1]);
+                        r.Name = split[2];
+                        r.Score = int.Parse(split[3]);
+                        Runners.Add(r);
+                    }
+                    Runners = new ObservableCollection<Player>(Runners.OrderByDescending(t => t.Score));
+                    for (int i = 0; i < Runners.Count; i++)
+                    {
+                        Runners[i].Rank = i + 1;
+                    }
+                }
             }
         }
     }
