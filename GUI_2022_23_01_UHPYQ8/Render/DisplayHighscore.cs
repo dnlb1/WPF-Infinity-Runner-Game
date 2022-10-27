01e2e59510a4135938af2b24fd319e1e81d785c3
Fekete Dominik <feketedominik@stud.uni-obuda.hu>
2022-10-27 09:25:07 +0200

    variable created

--- GUI_2022_23_01_UHPYQ8/Render/DisplayHighscore.cs
+++ GUI_2022_23_01_UHPYQ8/Render/DisplayHighscore.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace GUI_2022_23_01_UHPYQ8.Render
 {
-    internal class DisplayHighscore
+    public class DisplayHighscore : FrameworkElement
     {
+        Size size { get; set; }
+        public MediaPlayer player = new MediaPlayer();
+
+        ImageBrush brush
+        {
+            get
+            {
+                return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "scroll.jpg"), UriKind.RelativeOrAbsolute)));
+            }
+        }
     }
 }
