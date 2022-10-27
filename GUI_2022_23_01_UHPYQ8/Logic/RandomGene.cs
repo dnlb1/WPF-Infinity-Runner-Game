f859320b38cec3cd3f5b139247a3956fe032b34e
Bencze Dániel <rgrengarhyped@gmail.com>
2022-10-27 13:28:26 +0200

    Random added

---
+++ GUI_2022_23_01_UHPYQ8/Logic/RandomGene.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_2022_23_01_UHPYQ8.Logic
+{
+    class RandomGene
+    {
+        public static Random r = new Random();
+    }
+}
