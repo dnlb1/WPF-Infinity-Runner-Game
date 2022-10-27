fd0ac694bfa897298833ee94d7fba3e6e52bcd5a
Sörös Bence <bence.soros@stud.uni-obuda.hu>
2022-10-27 10:50:29 +0200

    YourName xaml created

---
+++ GUI_2022_23_01_UHPYQ8/YourName.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI_2022_23_01_UHPYQ8
+{
+    /// <summary>
+    /// Interaction logic for YourName.xaml
+    /// </summary>
+    public partial class YourName : Window
+    {
+        public YourName()
+        {
+            InitializeComponent();
+        }
+    }
+}
