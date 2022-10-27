49b113408f4b0bff3dd66fb0b85c2426ec5485d6
Bencze Dániel <rgrengarhyped@gmail.com>
2022-10-27 14:06:28 +0200

    GameScore View model

---
+++ GUI_2022_23_01_UHPYQ8/ViewModel/GameScoreViewModel.cs
@@ -0,0 +1,24 @@
+﻿using GUI_2022_23_01_UHPYQ8.Model;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_2022_23_01_UHPYQ8.ViewModel
+{
+    public class GameScoreViewModel : ObservableRecipient
+    {
+        public Player py { get; set; }
+
+        public void SetUp(Player py)
+        {
+            this.py = py;
+        }
+        public GameScoreViewModel()
+        {
+
+        }
+    }
+}
