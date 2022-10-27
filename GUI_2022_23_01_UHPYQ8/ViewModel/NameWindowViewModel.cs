fdd30449902a1ef345dee76de4bdb04dc9a16452
Sörös Bence <bence.soros@stud.uni-obuda.hu>
2022-10-27 10:56:38 +0200

    isDesign bool

--- GUI_2022_23_01_UHPYQ8/ViewModel/NameWindowViewModel.cs
+++ GUI_2022_23_01_UHPYQ8/ViewModel/NameWindowViewModel.cs
@@ -2,9 +2,11 @@
 using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace GUI_2022_23_01_UHPYQ8.ViewModel
@@ -17,10 +19,22 @@ namespace GUI_2022_23_01_UHPYQ8.ViewModel
         public string EnteredName
         {
             get { return enteredName; }
-            set { enteredName = value;  }
-
+            set 
+            { 
+                enteredName = value; 
+                (Ok as RelayCommand).NotifyCanExecuteChanged(); 
+            }
         }
 
         public ICommand Ok { get; set; }
+
+        public static bool IsInDesign
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
     }
 }
