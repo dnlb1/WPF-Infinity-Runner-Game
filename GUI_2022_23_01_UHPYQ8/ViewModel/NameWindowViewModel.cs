c1c6a5b8b5d2a2823861c841b8c63f0e80f2f46f
Sörös Bence <bence.soros@stud.uni-obuda.hu>
2022-10-27 10:54:07 +0200

    ISPressed

--- GUI_2022_23_01_UHPYQ8/ViewModel/NameWindowViewModel.cs
+++ GUI_2022_23_01_UHPYQ8/ViewModel/NameWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace GUI_2022_23_01_UHPYQ8.ViewModel
 {
-    public class NameWindowViewModel
+    public class NameWindowViewModel : ObservableRecipient
     {
+        public bool IsPressed;
     }
 }
